86bbe25be3d5e8fbe4e3488dcccb45c0a38e037b
piesieomane <piesieyeboah@gmail.com>
2022-08-31 20:11:44 +0300

    fix png

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -16,8 +16,22 @@ const CountryDetails = () => {
         <li key={country?.name.common}>
           <h1>{country?.name.common}</h1>
           <img src={country?.flags.png} alt="flag" />
-          <p>{country?.capital}</p>
-          <p>{country?.subregion}</p>
+
+          <p>
+            The Capital of {country?.name.common} is{' '}
+            <strong>{country?.capital}</strong>
+          </p>
+          <p>
+            And it has a population of <strong>{country?.population}</strong>{' '}
+            people
+          </p>
+          <p>
+            It is located in the subregion of{' '}
+            <strong>{country?.subregion}</strong>
+          </p>
+          <p>
+            The languages they speak are {Object.values(country?.languages)}
+          </p>
         </li>
       ))}
     </div>
