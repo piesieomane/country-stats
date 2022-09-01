cbb3080377b718fee2b7763e44d40905982f3bb3
piesieomane <piesieyeboah@gmail.com>
2022-09-01 12:11:35 +0300

    Style the worldpage

--- src/components/WorldPage.js
+++ src/components/WorldPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
-
 import Continents from './Continents';
+import '../styles/worldpage.scss';
 
 const WorldPage = () => {
   return (
     <>
       <div className="world_map">
-        <img src="/images/world.svg" alt="world-image" />
+        <div className="world_map_container">
+          <img src="/images/world.svg" alt="world-image" />
+          <h2>THE WORLD MAP</h2>
+        </div>
         <div className="stats">
-          <p>STATS BY CONTINENTS</p>
+          <h3>STATS BY CONTINENTS</h3>
         </div>
       </div>
 
