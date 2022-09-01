42e1c815441b5a3315a73fb2e7f69c0efe67fab7
piesieomane <piesieyeboah@gmail.com>
2022-09-01 12:03:17 +0300

    Style navbar with sass

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaMicrophone } from 'react-icons/fa';
 import { AiTwotoneSetting, AiFillLeftSquare } from 'react-icons/ai';
+import '../styles/navbar.scss';
 
 const Navbar = () => {
   return (
-    <header>
-      <div className="nav">
+    <header className="nav_bar">
+      <div className="nav_left">
         <NavLink to="/">
           <AiFillLeftSquare />
         </NavLink>
-        <h2>STATS OF COUNTRIES</h2>
-        <div>
-          <FaMicrophone />
-          <AiTwotoneSetting />
-        </div>
+      </div>
+      <h2>STATS OF COUNTRIES</h2>
+      <div className="nav_right">
+        <FaMicrophone />
+        <AiTwotoneSetting />
       </div>
     </header>
   );
